ffdb0a7c17aaefa18d9093aa9ef16ed19d271e99
IUT GEII Toulon Table 6 <qsdqsdq>
2020-09-29 12:28:47 +0200

    TP1

---
+++ Carte_moteur_provost_romensky.X/ToolBox.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ToolBox.h
+ * Author: TABLE 6
+ *
+ * Created on 29 septembre 2020, 12:22
+ */
+
+#ifndef TOOLBOX_H
+#define	TOOLBOX_H
+#define PI 3.141592653589793
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TOOLBOX_H */
+
